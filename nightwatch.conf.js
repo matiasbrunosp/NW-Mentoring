853fb4828405e5027c737407752ce38f0f0e2d5f
matiasbrunosp <mbruno@epix.com>
2020-06-29 20:13:54 -0300

    nightwatch.conf.js update

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,17 +1,28 @@
 module.exports = {
-    "src_folders": ["src/tests"],
-    "page_objects_path": ["src/pages"],
+    src_folders: ["src/tests"],
+    page_objects_path: ["src/pages"],
 
-    "webdriver": {
-        "start_process": true,
-        "server_path": "node_modules/.bin/chromedriver",
-        "port": 9515
+    webdriver: {
+        start_process: true,
+        server_path: require('chromedriver').path,
+        port: 9515
     },
 
-    "test_settings": {
-        "default": {
-            "desiredCapabilities": {
-                "browserName": "chrome"
+    test_settings: {
+        default: {
+            desiredCapabilities: {
+                browserName: "chrome"
+            },
+            globals: {
+                waitForConditionTimeout: 5000
+            },
+        },
+        headless: {
+            desiredCapabilities: {
+                browserName: "chrome",
+                chromeOptions: {
+                    args: ['-headless'],
+                }
             }
         }
     }
